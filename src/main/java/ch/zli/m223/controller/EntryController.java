d0255de8cbfeb7c8a2ddf99d058329f4105db66f
FabioKaelin <supermae206@gmail.com>
2023-08-29 09:19:03 +0200

    endpoints and test

--- src/main/java/ch/zli/m223/controller/EntryController.java
+++ src/main/java/ch/zli/m223/controller/EntryController.java
@@ -4,14 +4,17 @@ import java.util.List;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+import org.jboss.resteasy.reactive.RestPath;
 
 import ch.zli.m223.model.Entry;
 import ch.zli.m223.service.EntryService;
@@ -36,6 +39,23 @@ public class EntryController {
     @Operation(summary = "Creates a new entry.", description = "Creates a new entry and returns the newly added entry.")
     public Entry create(Entry entry) {
        return entryService.createEntry(entry);
+    }
+
+
+    @DELETE
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Delete a entry.", description = "Delete a entry.")
+    public void delete(@RestPath long id) {
+       entryService.deleteEntry(id);
+    }
+
+    @PUT
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Update a entry.", description = "Update a entry and returns the updated entry.")
+    public Entry update(Entry entry) {
+       return entryService.updateEntry(entry);
     }
 
 }
--- src/test/java/ch/zli/m223/EntryResourceTest.java
+++ src/test/java/ch/zli/m223/EntryResourceTest.java
@@ -1,15 +1,22 @@
 package ch.zli.m223;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class) // Specify the order of test methods
 public class EntryResourceTest {
 
     @Test
+    @Order(1)
     public void testIndexEndpoint() {
         given()
           .when().get("/entries")
@@ -17,5 +24,60 @@ public class EntryResourceTest {
              .statusCode(200)
              .body(is("[]"));
     }
+    // testCreateEndpoint with BODY
+    @Test
+    @Order(2)
+    public void testaaaaaCreateEndpoint() {
+        given().contentType(ContentType.JSON)
+        .body("{\"checkIn\": \"2022-03-10T12:15:50\",\"checkOut\": \"2022-03-10T12:15:50\"}")
+          .when().post("/entries")
+          .then()
+             .statusCode(200)
+             .body(is("{\"id\":1,\"checkIn\":\"2022-03-10T12:15:50\",\"checkOut\":\"2022-03-10T12:15:50\"}"));
+    }
+    // @Test
+    // public void testCreateEndpointFail() {
+    //     given().contentType(ContentType.JSON)
+    //       .when().post("/entries")
+    //       .then()
+    //          .statusCode(500);
+    // }
 
+    // testDeleteEndpoint with ID
+    // @Test
+    // public void testDeleteEndpoint() {
+
+    //     given().contentType(ContentType.JSON)
+    //       .when().delete("/entries/1")
+    //       .then()
+    //          .statusCode(204);
+    // }
+
+    // testDeleteEndpoint but insert before test a new entry
+    @Test
+    @Order(3)
+    public void testDeleteEndpoint() {
+        given().contentType(ContentType.JSON)
+        .body("{\"checkIn\": \"2022-03-10T12:15:50\",\"checkOut\": \"2022-03-10T12:15:50\"}")
+          .when().post("/entries")
+          .then()
+             .statusCode(200)
+             .body(is("{\"id\":2,\"checkIn\":\"2022-03-10T12:15:50\",\"checkOut\":\"2022-03-10T12:15:50\"}"));
+
+        given()
+          .when().delete("/entries/2")
+          .then()
+             .statusCode(204);
+    }
+
+    @Test
+    @Order(4)
+    public void testEditEndpoint() {
+        given().contentType(ContentType.JSON)
+                .body("{\"id\":3,\"checkIn\":\"2022-03-11T12:15:50\",\"checkOut\": \"2022-03-10T12:15:50\"}")
+                .when().put("/entries")
+                .then()
+                .statusCode(200)
+                .body(is("{\"id\":3,\"checkIn\":\"2022-03-11T12:15:50\",\"checkOut\":\"2022-03-10T12:15:50\"}"));
+    }
 }
